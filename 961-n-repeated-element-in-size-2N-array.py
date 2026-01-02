0629c89bc52ec6c711916bd581a72f3d78303a46
Shravya D'souza <117933075+ShravyaDsouza@users.noreply.github.com>
2026-01-02 21:12:18 +0530

    Update 961-n-repeated-element-in-size-2N-array.py

--- 961-n-repeated-element-in-size-2N-array.py
+++ 961-n-repeated-element-in-size-2N-array.py
@@ -1,8 +1,11 @@
-def repeatedNTimes(self, nums: List[int]) -> int:
+from typing import List
+
+def repeatedNTimes(nums: List[int]) -> int:
     result = set()
-    for num in nums :
+    for num in nums:
         if num in result:
             return num
         result.add(num)
-nums = [int(x.strip()) for x in user_input.split(",")]
+
+nums = [int(x) for x in input("Enter numbers: ").split(",")]
 print(f"Result: {repeatedNTimes(nums)}")
