9243b334948fe596a3bbe6d0278d047311055b1d
Shravya D'souza <117933075+ShravyaDsouza@users.noreply.github.com>
2026-01-09 11:38:44 +0530

    Create 865_smallest_subtree_with_all_the_deepest_nodes.py

---
+++ 865_smallest_subtree_with_all_the_deepest_nodes.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(node):
+            if not node:
+                return 0, None
+            
+            l_depth, l_node = dfs(node.left)
+            r_depth, r_node = dfs(node.right)
+
+            if l_depth > r_depth:
+                return l_depth + 1, l_node
+            
+            if r_depth > l_depth:
+                return r_depth + 1, r_node
+
+            return l_depth + 1, node
+
+        final_depth, result_node = dfs(root)
+        return result_node
