bd1d96499babe52dd8d057245ede3c532730b2f7
Shravya D'souza <117933075+ShravyaDsouza@users.noreply.github.com>
2026-06-05 20:33:39 +0530

    Implement Reverse Polish Notation evaluation

---
+++ evaluate_reverse_polish_notation.py
@@ -0,0 +1,19 @@
+def evalRPN( tokens: List[str]) -> int:
+  stack = []
+  for t in tokens :
+      if t in ("*","+","-","/"):
+          b = stack.pop()
+          a = stack.pop()
+                
+          if t == "*":
+              tack.append(a*b)
+          if t == "+":
+              stack.append(a+b)
+          if t == "-":
+              stack.append(a-b)
+          if t == "/":
+              stack.append(int(a/b))
+        else :
+            stack.append(int(t))
+      
+  return stack[0]
